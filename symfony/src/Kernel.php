e8b9c9fd3051e540141f1d09495a625dec71a4ff
Carles <carles.blavi@gmail.com>
2026-08-18 22:49:35 +0200

    chore: Symfony basic installation
    - Installed symfony 8.1 and created the initial lucky/number route
    - Moved symfony into its own folder
    - Reorganized folder structure to split the project into: Domain, Application and Symfony to improve clarity.

---
+++ symfony/src/Kernel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Symfony;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    public function getProjectDir(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    /**
+     * @return list<string> An array of allowed values for APP_ENV
+     */
+    private function getAllowedEnvs(): array
+    {
+        return ['prod', 'dev', 'test'];
+    }
+}
